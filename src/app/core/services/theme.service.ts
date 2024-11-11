21660941b17c49972ed93937acc39fe79680706d
robert-portilho <31190637+robert-portilho@users.noreply.github.com>
2024-11-11 20:52:46 +0000

    theme service

---
+++ src/app/core/services/theme.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+export interface Theme {
+  name: string;
+  displayName: string;
+  accent?: string;
+  primary?: string;
+  isDark?: boolean;
+  isDefault?: boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeService {
+  static defaultTheme: Theme =   {
+    displayName: 'Light theme',
+    name: 'light-theme',
+    isDark: false,
+    isDefault: true,
+  };
+
+  themes: Theme[] = [
+    {
+      displayName: 'Orange theme',
+      name: 'orange-theme',
+      isDark: false,
+    },
+    {
+      displayName: 'Dark theme',
+      name: 'dark-theme',
+      isDark: true,
+    },
+    ThemeService.defaultTheme
+  ];
+
+  private themeSUB = new BehaviorSubject(ThemeService.defaultTheme); // stores the current theme
+  themeOBS = this.themeSUB.asObservable();
+
+  constructor() {
+  }
+
+  updateTheme(theme: Theme): void {
+    this.themeSUB.next(theme);
+  }
+
+  findTheme(themeName: string): Theme | undefined {
+    return this.themes.find(currentTheme => currentTheme.name === themeName);
+  }
+}
